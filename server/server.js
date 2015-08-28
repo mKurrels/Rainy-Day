66c33b80310b314689728ccfc3cb15e84c9faf02
mKurrels <mjkurrels@gmail.com>
2015-08-28 10:55:44 -0700

    auth started

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/auth', function (req, res) {
+  res.redirect('https://sandbox-api.venmo.com/v1/oauth/authorize?client_id=2887&scope=access_profile');
+});
+
 var port = 3000;
 
 app.listen(port);

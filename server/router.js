1ffb5a9df0292b9c00625a4cc57940e276c492df
mKurrels <mjkurrels@gmail.com>
2015-08-31 11:37:48 -0700

    use router.route() to simplify router

--- server/router.js
+++ server/router.js
@@ -14,11 +14,13 @@ router.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-router.post('/users', reqHandlers.postUser);
-router.get('/users', reqHandlers.getAllUsers);
+router.route('/users')
+  .get(reqHandlers.getAllUsers)
+  .post(reqHandlers.postUser);
 
-router.post('/pots', reqHandlers.postUser);
-router.get('/pots', reqHandlers.getAllUsers);
+router.route('/pots')
+  .post(reqHandlers.postUser)
+  .get(reqHandlers.getAllUsers);
 
 router.get('/auth', function (req, res) {
   res.redirect('https://sandbox-api.venmo.com/v1/oauth/authorize?client_id=2887&scope=access_profile');

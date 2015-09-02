149ca68d8c690fd9ba112438506303998c3b3f24
mKurrels <mjkurrels@gmail.com>
2015-09-02 11:16:51 -0700

    use morgon to log requests

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ var app = express();
 var reqHandlers = require('./request_handlers');
 var bodyParser = require('body-parser');
 var api = require('./router');
+var morgan = require('morgan');
 
 
 app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json());
 app.use('/api', api);
+app.use(morgan('combined'));
 
 
 var port = 3000;
 app.listen(port);
 module.exports = app;
+
 
 console.log('family fund server listening on port ' + port);

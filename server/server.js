a9fb7a9263285fd6853677ee2349373325f78145
mKurrels <mjkurrels@gmail.com>
2015-09-08 16:52:36 -0700

    app uses https now

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 var express = require('express');
-var app = express();
+var https = require('https');
 var reqHandlers = require('./request_handlers');
 var bodyParser = require('body-parser');
 var api = require('./router');
 var morgan = require('morgan');
-
+var app = express();
+var fs = require('fs');
 
 app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
 app.use('/api', api);
+app.use(morgan('dev'));
+
+var options = {
+  key: fs.readFileSync(__dirname + '/../key.pem'),
+  cert: fs.readFileSync(__dirname + '/../cert.pem')
+};
+
+https.createServer(options, app).listen(8443);
 
 
-var port = 3000;
-app.listen(port);
+
+
+
+
 module.exports = app;
-
-
-console.log('family fund server listening on port ' + port);

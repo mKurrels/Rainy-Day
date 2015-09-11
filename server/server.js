98e5305c80179eaabaa95957df68aa98c37b599f
mKurrels <mjkurrels@gmail.com>
2015-09-10 17:30:08 -0700

    try to add postgress connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 var https = require('https');
 var fs = require('fs');
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 var app = require('./server_config');
 
